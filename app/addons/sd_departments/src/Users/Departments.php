445cb85cf39a911af17e381b51287cbf485065b0
Danendz <danilapayne@gmail.com>
2023-05-16 14:58:41 +0500

    SCHOOL 2: Реализовал backend контролер и необходимые функции для модуля Departments

---
+++ app/addons/sd_departments/src/Users/Departments.php
@@ -0,0 +1,460 @@
+<?php
+
+/***************************************************************************
+ *                                                                          *
+ *   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
+ *                                                                          *
+ * This  is  commercial  software,  only  users  who have purchased a valid *
+ * license  and  accept  to the terms of the  License Agreement can install *
+ * and use this program.                                                    *
+ *                                                                          *
+ ****************************************************************************
+ * PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
+ * "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
+ ****************************************************************************/
+
+namespace Tygh\Addons\SdDepartments\Users;
+
+use Tygh\Application;
+use Tygh\Database\Connection;
+use Tygh\Enum\ImagePairTypes;
+use Tygh\Enum\ObjectStatuses;
+use Tygh\Enum\SiteArea;
+use Tygh\Languages\Languages;
+use Tygh\Registry;
+use Tygh\Tygh;
+
+/**
+ * Class Users Departments
+ *
+ * @package Departments
+ */
+class Departments
+{
+    protected Connection $db;
+
+    protected string $area;
+
+    protected string $lang_code;
+
+    /**
+     * Departments constructor.
+     *
+     * @param Application $app
+     * @param string $area
+     * @param string $lang_code
+     */
+    public function __construct(Application $app, $area = AREA)
+    {
+        $this->db = $app['db'];
+        $this->area = $area;
+        $this->lang_code = AREA === SiteArea::STOREFRONT ? CART_LANGUAGE : DESCR_SL;
+    }
+
+    /**
+     * Get all departments matching parameters
+     *
+     * @param array $params query params
+     *
+     * @return array Department data
+     */
+    public function getList($params)
+    {
+        // Set default values to input params
+        $default_params = [
+            'page' => 1,
+            'items_per_page' => 0
+        ];
+
+        $params = array_merge($default_params, $params);
+
+        /**
+         * Executes at the beginning of the method, allowing you to modify the arguments passed to the method.
+         *
+         * @param string[] $params Params
+         */
+        fn_set_hook('get_departments_pre', $params);
+
+        $cache_key = __FUNCTION__ . md5(serialize($params));
+
+        Registry::registerCache(
+            $cache_key,
+            ['departments', 'department_descriptions'],
+            Registry::cacheLevel('locale_auth'),
+            true
+        );
+
+        $cache = Registry::get($cache_key);
+
+        if (!empty($cache)) {
+            $departments = $cache;
+        } else {
+            $fields = [
+                'id' => '?:departments.department_id',
+                'status' => '?:departments.status',
+                'timestamp' => '?:departments.timestamp',
+                'supervisor_id' => '?:departments.supervisor_id',
+                'name' => '?:department_descriptions.department',
+                'description' => '?:department_descriptions.description'
+            ];
+
+            $sortings = [
+                'timestamp' => '?:departments.timestamp',
+                'name' => '?:department_descriptions.department',
+                'status' => '?:departments.status',
+            ];
+
+            $condition = [];
+            $limit = '';
+            $join = '';
+
+            if (!empty($params['limit'])) {
+                $limit = $this->db->quote(' LIMIT 0, ?i', $params['limit']);
+            }
+
+            if (!empty($params['item_ids'])) {
+                $condition['item_ids'] = $this->db->quote(
+                    'AND ?:departments.department_id IN (?n)',
+                    explode(',', $params['item_ids'])
+                );
+            }
+
+            if (!empty($params['department_id'])) {
+                $condition['department_id'] = $this->db->quote(
+                    'AND ?:departments.department_id = ?i',
+                    $params['department_id']
+                );
+            }
+
+            if ($this->area === SiteArea::STOREFRONT) {
+                $condition['status'] = $this->db->quote(
+                    'AND ?:departments.status = ?s',
+                    ObjectStatuses::ACTIVE
+                );
+            } elseif (!empty($params['status'])) {
+                $condition['status'] = $this->db->quote(
+                    'AND ?:departments.status = ?s',
+                    $params['status']
+                );
+            }
+
+            if (!empty($params['department_name'])) {
+                $condition['department_name'] = $this->db->quote(
+                    'AND ?:department_descriptions.department LIKE ?l',
+                    '%' . $params['department_name'] . '%'
+                );
+            }
+
+            if (!empty($params['supervisor_id'])) {
+                $condition['supervisor_id'] = $this->db->quote(
+                    'AND ?:departments.supervisor_id = ?s',
+                    $params['supervisor_id']
+                );
+            }
+
+            $join .= $this->db->quote(
+                ' LEFT JOIN ?:department_descriptions' .
+                    ' ON ?:department_descriptions.department_id = ?:departments.department_id' .
+                    ' AND ?:department_descriptions.lang_code = ?s',
+                $this->lang_code
+            );
+
+            if (!empty($params['items_per_page'])) {
+                $params['total_items'] = $this->db->getField(
+                    'SELECT COUNT(*) FROM ?:departments ?p WHERE 1 ?p',
+                    $join,
+                    implode(' ', $condition)
+                );
+
+                $limit = db_paginate(
+                    $params['page'],
+                    $params['items_per_page'],
+                    $params['total_items']
+                );
+            }
+
+            /**
+             * Prepare params for getting departments query
+             *
+             * @param string[] $params Params
+             * @param string[] $fields Query fields
+             * @param string[] $condition Query conditions
+             * @param string[] $sortings Query sortings params
+             */
+            fn_set_hook('get_departments', $params, $fields, $condition, $sortings);
+
+            $sorting = db_sort($params, $sortings, 'name', 'asc');
+
+            $departments = $this->db->getHash(
+                'SELECT ?p FROM ?:departments ' .
+                    $join .
+                    'WHERE 1 ?p ?p ?p',
+                'department_id',
+                implode(', ', $fields),
+                implode(' ', $condition),
+                $sorting,
+                $limit
+            );
+        }
+
+        $department_image_ids = array_keys($departments);
+        $images = fn_get_image_pairs(
+            $department_image_ids,
+            'department',
+            ImagePairTypes::MAIN,
+            true,
+            false,
+            $this->lang_code
+        );
+
+        foreach ($departments as $department_id => $_department) {
+            $departments[$department_id]['main_pair'] = !empty($images[$department_id])
+                ? reset($images[$department_id])
+                : [];
+        }
+
+        /**
+         * Actions after getting departments list
+         *
+         * @param array $departments Departments list
+         * @param array $params Params list
+         */
+        fn_set_hook('get_departments_post', $users, $params);
+
+        return [$departments, $params];
+    }
+
+    /**
+     * Get department by id
+     *
+     * @param int $department_id Id of the department
+     *
+     * @return array Department data
+     */
+    public function get($department_id = 0)
+    {
+        $department = [];
+        if (!empty($department_id)) {
+            [$departments] = $this->getList([
+                'department_id' => $department_id
+            ]);
+
+            if (!empty($departments)) {
+                $department = reset($departments);
+                $department['employee_ids'] = $this->getLinks($department['department_id']);
+            }
+        }
+        return $department;
+    }
+
+    /**
+     * Update department/Add department by id
+     *
+     * @param array $data New department data
+     *
+     * @param int $department_id Id of the department
+     *
+     * @return mixed department_id or false
+     */
+    public function upsert($data, $department_id)
+    {
+        array_walk($data, 'fn_trim_helper');
+
+        $is_department_empty = empty($data['department']);
+        $is_supervisor_empty = empty($data['supervisor_id']);
+
+        if ($is_department_empty || $is_supervisor_empty) {
+            fn_set_notification(
+                'E',
+                __('error'),
+                __('text_fill_the_mandatory_fields')
+            );
+            return false;
+        }
+
+        if (!empty($department_id)) {
+            $this->db->query(
+                'UPDATE ?:departments SET ?u WHERE department_id = ?i',
+                $data,
+                $department_id
+            );
+
+            $this->db->query(
+                'UPDATE ?:department_descriptions SET ?u WHERE department_id = ?i ' .
+                    'AND lang_code = ?s',
+                $data,
+                $department_id,
+                $this->lang_code
+            );
+        } else {
+            $data['timestamp'] = time();
+            $department_id = $data['department_id'] = $this->db->replaceInto(
+                'departments',
+                $data
+            );
+
+            $data_with_languages = [];
+            foreach (Languages::getAll() as $lang_code => $_v) {
+                $data_with_languages[] = [
+                    'department_id' => $data['department_id'],
+                    'department' => $data['department'],
+                    'description' => $data['description'],
+                    'lang_code' => $lang_code
+                ];
+            }
+            $this->db->replaceInto(
+                'department_descriptions',
+                $data_with_languages,
+                true
+            );
+        }
+
+        if (!empty($department_id)) {
+            fn_attach_image_pairs(
+                'department',
+                'department',
+                $department_id,
+                $this->lang_code
+            );
+        }
+
+        $employee_ids = !empty($data['employee_ids']) ? $data['employee_ids'] : [];
+
+        $this->deleteLinks($department_id);
+        $this->addLinks(
+            $department_id,
+            $data['supervisor_id'],
+            $employee_ids
+        );
+        return $department_id;
+    }
+
+    /**
+     * Get users with only necessary fields
+     *
+     * @param $params Params
+     * @param $userIds Ids of needed users
+     */
+    public function getUsers($params, $userIds)
+    {
+        $params['user_id'] = $userIds;
+
+        // It is necessary to query for only needed users fields through hook
+        $params['sd_departments_users_field_limit'] = true;
+
+        return fn_get_users($params, Tygh::$app['session']['auth']);
+    }
+
+    /**
+     * Deletes all images pairs
+     *
+     * @return void
+     */
+    public function deleteAllImagePairs()
+    {
+        $department_ids = $this->getAllIds();
+
+        foreach ($department_ids as $department_id) {
+            fn_delete_image_pairs(
+                $department_id,
+                'department',
+                ImagePairTypes::MAIN
+            );
+        }
+    }
+
+    /**
+     * Remove department by id
+     *
+     * @param int $department_id Id of the department
+     *
+     * @return void
+     */
+    public function delete($department_id)
+    {
+        if (!empty($department_id)) {
+            $this->db->query(
+                'DELETE FROM ?:departments WHERE department_id = ?i',
+                $department_id
+            );
+
+            fn_delete_image_pairs(
+                $department_id,
+                'department',
+                ImagePairTypes::MAIN
+            );
+
+            $this->deleteLinks($department_id);
+        }
+    }
+
+    /**
+     * Get department links by id
+     *
+     * @param int $department_id Id of the department
+     *
+     * @return array Department links
+     */
+    protected function getLinks($department_id)
+    {
+        return !empty($department_id) ? $this->db->getColumn(
+            'SELECT employee_id FROM ?:department_links WHERE department_id = ?i',
+            $department_id
+        ) : [];
+    }
+
+    /**
+     * Add department links by id
+     *
+     * @param int $department_id Id of the department
+     *
+     * @param int $supervisor_id Id of the department supervisor
+     *
+     * @param string $employee_ids Employee ids of the current department
+     *
+     * @return void
+     */
+    protected function addLinks($department_id, $supervisor_id, $employee_ids)
+    {
+        if (!empty($employee_ids)) {
+            $employee_ids = explode(',', $employee_ids);
+            $department_links = [];
+            foreach ($employee_ids as $employee_id) {
+                if ($employee_id !== $supervisor_id) {
+                    $department_links[] = [
+                        'department_id' => $department_id,
+                        'employee_id' => $employee_id
+                    ];
+                }
+            }
+            $this->db->replaceInto('department_links', $department_links, true);
+        }
+    }
+
+    /**
+     * Remove department links by id
+     *
+     * @param int $department_id Id of the department
+     *
+     * @return void
+     */
+    protected function deleteLinks($department_id)
+    {
+        if (!empty($department_id)) {
+            $this->db->query(
+                'DELETE FROM ?:department_links WHERE department_id = ?i',
+                $department_id
+            );
+        }
+    }
+
+    /**
+     * Gets all department ids
+     *
+     * @return array ids
+     */
+    protected function getAllIds()
+    {
+        return $this->db->getColumn('SELECT department_id FROM ?:departments WHERE 1');
+    }
+}
---
+++ app/addons/sd_departments/src/controllers/backend/profiles.post.php
@@ -0,0 +1,99 @@
+<?php
+
+/***************************************************************************
+ *                                                                          *
+ *   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
+ *                                                                          *
+ * This  is  commercial  software,  only  users  who have purchased a valid *
+ * license  and  accept  to the terms of the  License Agreement can install *
+ * and use this program.                                                    *
+ *                                                                          *
+ ****************************************************************************
+ * PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
+ * "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
+ ****************************************************************************/
+
+use Tygh\Registry;
+use Tygh\Tygh;
+use Tygh\Addons\SdDepartments\ServiceProvider;
+
+defined('BOOTSTRAP') or die('Access denied');
+
+$departments_service = ServiceProvider::getDepartmentsService();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($mode === 'update_department') {
+        $department_id = !empty($_REQUEST['department_id'])
+            ? $_REQUEST['department_id']
+            : 0;
+
+        $data = !empty($_REQUEST['department_data'])
+            ? $_REQUEST['department_data']
+            : [];
+
+        $res = $departments_service->upsert($data, $department_id);
+
+        $url = 'profiles.';
+
+        if ($res === false) {
+            $url .= empty($department_id)
+                ? 'add_department'
+                : "update_department&department_id=$department_id";
+        } else {
+            $url .= 'manage_departments';
+        }
+
+        return [CONTROLLER_STATUS_OK, $url];
+    }
+
+    if ($mode === 'delete_department') {
+        $department_id = !empty($_REQUEST['department_id'])
+            ? $_REQUEST['department_id']
+            : 0;
+
+        $departments_service->delete($department_id);
+        fn_set_notification('N', __('notice'), __('sd_departments_success_delete_one'));
+        return [CONTROLLER_STATUS_OK, 'profiles.manage_departments'];
+    }
+
+    if ($mode === 'delete_departments') {
+        if (!empty($_REQUEST['departments_ids'])) {
+            foreach ($_REQUEST['departments_ids'] as $department_id) {
+                $departments_service->delete($department_id);
+            }
+        }
+
+        fn_set_notification('N', __('notice'), __('sd_departments_success_delete_multiple'));
+        return [CONTROLLER_STATUS_OK, 'profiles.manage_departments'];
+    }
+}
+
+if ($mode === 'update_department' || $mode === 'add_department') {
+    $department_id = !empty($_REQUEST['department_id'])
+        ? $_REQUEST['department_id']
+        : 0;
+
+    $department_data = $departments_service->get($department_id);
+
+    if (empty($department_data) && $mode === 'update_department') {
+        return [CONTROLLER_STATUS_NO_PAGE];
+    }
+
+    Tygh::$app['view']->assign([
+        'department_data' => $department_data,
+        'supervisor_info' => !empty($department_data['supervisor_id'])
+            ? fn_get_user_short_info($department_data['supervisor_id'])
+            : 0
+    ]);
+} elseif ($mode === 'manage_departments') {
+    $params = $_REQUEST;
+    $params['items_per_page'] = $_REQUEST['items_per_page'] ??
+        Registry::get('settings.Appearance.admin_elements_per_page');
+
+    [$departments, $search] = $departments_service->getList($params);
+
+    Tygh::$app['view']->assign([
+        'departments' => $departments,
+        'search' => $search
+    ]);
+}
